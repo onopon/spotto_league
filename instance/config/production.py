fab1b903d379f598a12e33d73ded8ff13b106bd8
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-17 23:13:37 +0900

    fix config for production
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- instance/config/production.py
+++ instance/config/production.py
@@ -2,10 +2,10 @@ import os
 
 # flask
 DEBUG = False
-USER = 'spotto'
-PASSWORD = 'spotto'
+USER = os.environ.get('DB_NAME')
+PASSWORD = os.environ.get('DB_PASSWORD')
 HOST = os.environ.get('DATABASE_URL')
-DB = 'spotto'
+DB = os.environ.get('DB_NAME')
 
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/flask_sample?charset=utf8'.format(**{
