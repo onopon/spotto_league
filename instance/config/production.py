56134709d538ac54b7538ba978c96e8d918e1d92
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-18 00:07:30 +0900

    fix db config
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- instance/config/production.py
+++ instance/config/production.py
@@ -9,9 +9,9 @@ DB = os.environ.get('DB_NAME')
 
 # SQLAlchemy
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/flask_sample?charset=utf8'.format(**{
-    'user': os.getenv('DB_USER', USER),
-    'password': os.getenv('DB_PASSWORD', PASSWORD),  # あとで変える（何に変えるのが適切なんだろ）
-    'host': os.getenv('DB_HOST', HOST),
+    'user': USER,
+    'password': PASSWORD,
+    'host': HOST,
     'db': DB
 })
 SQLALCHEMY_TRACK_MODIFICATIONS = False
