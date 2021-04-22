84e23c823fc2d30c6070c7451f0a335537146be0
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-22 23:37:34 +0900

    fix calculator
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- spotto_league/modules/league_settlement_calculator.py
+++ spotto_league/modules/league_settlement_calculator.py
@@ -9,16 +9,8 @@ class LeagueSettlementCalculator():
     @classmethod
     def get_settlement_hash(cls, members: List[LeagueMember], logs: List[LeagueLog]):
         settlement_hash = defaultdict(lambda: dict())
-        count_of_games = len(members) - 1  # 初期値は出来うる試合数の最大値としておく
-        # ここで選手ごとにlogを分ける
         for member in members:
             member_logs = [l for l in logs if l.is_in_user_id(member.user_id)]
-#            if count_of_games > len(member_logs):
-#                count_of_games = len(member_logs)
-            settlement_hash[member.user_id]['logs'] = member_logs
-
-        for member in members:
-            member_logs = settlement_hash[member.user_id]['logs'][:count_of_games]
             settlement_hash[member.user_id]['logs'] = member_logs
             settlement_hash[member.user_id]['details'] = LeagueLogDetail.find_all_by_league_log_ids([l.id for l in member_logs])
         return settlement_hash
