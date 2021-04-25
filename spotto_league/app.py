1bc0d977292e0f9c7097bd91838675f2af8edf04
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-25 12:40:09 +0900

    add requirements and fix app.py
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- spotto_league/app.py
+++ spotto_league/app.py
@@ -9,15 +9,11 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     # 標準設定ファイル読み込み
     app.config.from_object("settings")
-    environment = os.environ.get('ENV', app.config["DEFAULT_ENV"])
-    secret_key = os.environ.get('SECRET_KEY', app.config["DEFAULT_SECRET_KEY"]).encode('utf-8')
+    environment = app.config['ENV']
+    secret_key = app.config['SECRET_KEY'].encode('utf-8')
 
     # 非公開設定ファイル読み込み
-    app.config.from_pyfile(os.path.join("config", "common.py"), silent=True)
-    if environment == "development":
-        app.config.from_pyfile(os.path.join("config", "development.py"), silent=True)
-    else:
-        app.config.from_pyfile(os.path.join("config", "production.py"), silent=True)
+    app.config.from_pyfile(os.path.join("config", "db.py"), silent=True)
 
     init_db(app)
     app.secret_key = secret_key
