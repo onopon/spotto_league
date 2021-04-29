6d8f1f87811af1ed29d790a6a4bba7369c0c6907
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-30 00:18:05 +0900

    fix
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- static/js/apple-mobile-web-app.js
+++ static/js/apple-mobile-web-app.js
@@ -1,9 +1,11 @@
-$(document).on('click', 'a', function(e){
-  val = $(this).attr("href");
-  if (val.match(/^(http|https):\/\//i)) {
-    return true;
-  }
-  e.preventDefault();
-  location.href = $(this).attr('href') ;
-  return false;
+$(function() {
+  $(document).on('click', 'a', function(e){
+    val = $(this).attr("href");
+    if (val.match(/^(http|https):\/\//i)) {
+      return true;
+    }
+    e.preventDefault();
+    location.href = $(this).attr('href') ;
+    return false;
+  });
 });
