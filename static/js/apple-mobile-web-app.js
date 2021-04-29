c4fcb8c90adcbc3487e6c65b7a68977f6619618f
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-30 00:42:09 +0900

    fix js and base.html
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- static/js/apple-mobile-web-app.js
+++ static/js/apple-mobile-web-app.js
@@ -1,11 +1,31 @@
-$(function() {
-  $(document).on('click', 'a', function(e){
-    val = $(this).attr("href");
-    if (val.match(/^(http|https):\/\//i)) {
-      return true;
+// Initialization
+jQuery.mobile_js_link = {
+	init: function() {
+		for (module in jQuery.mobile_js_link) {
+			if (jQuery.mobile_js_link[module].init)
+				jQuery.mobile_js_link[module].init();
+		}
+	}
+};
+jQuery(document).ready(jQuery.mobile_js_link.init);
+jQuery.mobile_js_link.rewrite = {
+  init: function() {
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){}
+    else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){}
+    else{
+      return;
     }
-    e.preventDefault();
-    location.href = $(this).attr('href') ;
-    return false;
-  });
-});
+    var all_tags = $('a');
+    all_tags.each(function(){
+      var url = $(this).attr('href');
+      if (url.match(/^(http|https):\/\//i)) {
+        return;
+      }
+      $(this).attr('href','#');
+      $(this).click(function(){
+        location.href = url;
+      });
+    });
+  }
+};
