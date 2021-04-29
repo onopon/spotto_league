c0d19e0103700c350506d53333613789c1e2ace8
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-04-30 00:13:31 +0900

    fix ios setting
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

---
+++ static/js/apple-mobile-web-app.js
@@ -0,0 +1,9 @@
+$(document).on('click', 'a', function(e){
+  val = $(this).attr("href");
+  if (val.match(/^(http|https):\/\//i)) {
+    return true;
+  }
+  e.preventDefault();
+  location.href = $(this).attr('href') ;
+  return false;
+});
