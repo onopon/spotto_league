5a407b5bf9bc9b90c71dae8b3835ff61cf3c41c5
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2021-05-12 02:56:34 +0900

    fix
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- spotto_league/controllers/admin/league/league_cancel_controller.py
+++ spotto_league/controllers/admin/league/league_cancel_controller.py
@@ -1,23 +1,9 @@
 import asyncio
-import json
-from typing import Dict, Any, List
-from collections import defaultdict
 from spotto_league.controllers.base_controller import BaseController
 from werkzeug.wrappers import BaseRequest, BaseResponse
-from flask import Flask, request, render_template, redirect, url_for
-from flask_login import current_user
-from spotto_league.entities.rank import Rank
+from flask import request, redirect, url_for
 from spotto_league.models.league import League
-from spotto_league.models.league_member import LeagueMember
-from spotto_league.models.league_point import LeaguePoint
-from spotto_league.models.bonus_point import BonusPoint
-from spotto_league.models.league_log import LeagueLog
-from spotto_league.models.league_log_detail import LeagueLogDetail
-from spotto_league.models.user_point import UserPoint
-from spotto_league.database import db
 from ponno_linebot.ponno_bot import PonnoBot
-
-INVALID_MATCH_GROUP_ID = 0
 
 
 class LeagueCancelController(BaseController):
