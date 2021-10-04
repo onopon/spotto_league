294c3d54d9b9317b5d708646d828a79e69c1dd42
onopon <kiyopon.onopon@gmail.com>
2021-10-04 23:14:09 +0900

    line通知をするかしないかのチェックボタン (#77)
    * add check box for notify to line
    
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
    
    * fix flake8
    
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- tests/spotto_league/controllers/admin/league/test_post_league_finish_controller.py
+++ tests/spotto_league/controllers/admin/league/test_post_league_finish_controller.py
@@ -1,5 +1,6 @@
 from tests.spotto_league.controllers.base_controller import BaseController
 from tests.modules.data_creator import DataCreator
+from tests.modules.ponno_line_decorator import PonnoLineDecorator
 from spotto_league.controllers.admin.league.post_league_finish_controller import INVALID_MATCH_GROUP_ID
 from spotto_league.scripts.add_league_point import AddLeaguePoint
 from spotto_league.models.league_member import LeagueMember
@@ -13,7 +14,84 @@ URL_PATH = '/admin/league/{}/finish'
 
 
 class TestPostLeagueFinishController(BaseController):
+    decorator = PonnoLineDecorator()
+
     def test_normal(self, mocker):
+        # mocker と PonnoLineDecoratorを使おうとしたときの解決策がこれ以外考え付かなかった
+        @TestPostLeagueFinishController.decorator.line_pushed
+        def execute():
+            AddLeaguePoint().execute()
+            place = DataCreator().create('place')
+            league = DataCreator().create('default_league', overrided_dict={'place_id': place.id, 'status': LeagueStatus.READY.value})
+
+            admin_user = DataCreator().create('admin_user')
+            users = DataCreator().create('six_member_users')
+            guest = DataCreator().create('guest_user')
+            visitor = DataCreator().create('visitor_user')
+            users.append(guest)
+            users.append(visitor)
+            b_point = 1500
+            bonus_point = BonusPoint.find_or_initialize_by_user_id(users[1].id)
+            bonus_point.point = b_point
+            bonus_point.available_count = 4
+            bonus_point.save()
+            for u in users:
+                league_member = LeagueMember.find_or_initialize_by_league_id_and_user_id(league.id, u.id)
+                league_member.enabled = True
+                league_member.save()
+            user = users[2]
+            league_member = LeagueMember.find_or_initialize_by_league_id_and_user_id(league.id, user.id)
+
+            league_log_2 = LeagueLog.find_or_initialize(league.id, users[1].id, users[2].id)
+            league_log_2.save()
+
+            # users[2]を基準として見ているので、
+            # users[0] vs users[2] は 2-0でusers[0]の勝ちとなる
+            log_details_hash = {0: 'league_log_details_2_0',
+                                1: 'league_log_details_1_2',
+                                3: 'league_log_details_0_2',
+                                4: 'league_log_details_2_1',
+                                5: None}
+            for (index, yml_title) in log_details_hash.items():
+                league_log = LeagueLog.find_or_initialize(league.id, user.id, users[index].id)
+                league_log.save()
+                if yml_title:
+                    DataCreator().create(yml_title, overrided_dict={'league_log_id': league_log.id})
+
+            self.login(admin_user.login_name, 'password')
+            assert league.league_point_group_id is None
+
+            # 連勝記録
+            mocker.patch.object(ContinuousPoint, 'count_for_bonus', 3)
+
+            result = self.post(URL_PATH.format(league.id), {"league_point_group_id": 1, "should_notify": 1})
+            assert result.status_code == 302
+            point_ranks = PointRank.make_point_rank_list(league)
+            expected_hash_list = [
+                {'user_id': users[2].id, 'Total': 6000, 'LeaguePoint': [3000], 'BonusPoint': 1500, 'ContinuousPoint': 1500},
+                {'user_id': users[0].id, 'Total': 2000, 'LeaguePoint': [2000], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': users[3].id, 'Total': 1500, 'LeaguePoint': [1500], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': users[5].id, 'Total': 1200, 'LeaguePoint': [1200], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': users[4].id, 'Total': 800, 'LeaguePoint': [800], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': users[1].id, 'Total': 700, 'LeaguePoint': [700], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': admin_user.id, 'Total': 0, 'LeaguePoint': [], 'BonusPoint': 0, 'ContinuousPoint': 0},
+                {'user_id': guest.id, 'Total': 0, 'LeaguePoint': [], 'BonusPoint': 0, 'ContinuousPoint': 0},
+            ]
+            for i, point_rank in enumerate(point_ranks):
+                expected_hash = expected_hash_list[i]
+                assert point_rank.user.id == expected_hash['user_id']
+                assert point_rank.current_point == expected_hash['Total']
+                assert point_rank.current_league_points == expected_hash['LeaguePoint']
+                assert point_rank.current_league_points == expected_hash['LeaguePoint']
+                assert point_rank.current_league_points == expected_hash['LeaguePoint']
+                assert point_rank.current_bonus_point == expected_hash['BonusPoint']
+                assert point_rank.current_bonus_point == expected_hash['ContinuousPoint']
+            assert league.league_point_group_id == 1
+            assert LeagueStatus(league.status) == LeagueStatus.FINISHED
+        execute()
+
+    @decorator.line_not_pushed
+    def test_normal_line_not_pushed(self):
         AddLeaguePoint().execute()
         place = DataCreator().create('place')
         league = DataCreator().create('default_league', overrided_dict={'place_id': place.id, 'status': LeagueStatus.READY.value})
@@ -24,64 +102,16 @@ class TestPostLeagueFinishController(BaseController):
         visitor = DataCreator().create('visitor_user')
         users.append(guest)
         users.append(visitor)
-        b_point = 1500
-        bonus_point = BonusPoint.find_or_initialize_by_user_id(users[1].id)
-        bonus_point.point = b_point
-        bonus_point.available_count = 4
-        bonus_point.save()
         for u in users:
             league_member = LeagueMember.find_or_initialize_by_league_id_and_user_id(league.id, u.id)
             league_member.enabled = True
             league_member.save()
-        user = users[2]
-        league_member = LeagueMember.find_or_initialize_by_league_id_and_user_id(league.id, user.id)
-
-        league_log_2 = LeagueLog.find_or_initialize(league.id, users[1].id, users[2].id)
-        league_log_2.save()
-
-        # users[2]を基準として見ているので、
-        # users[0] vs users[2] は 2-0でusers[0]の勝ちとなる
-        log_details_hash = {0: 'league_log_details_2_0',
-                            1: 'league_log_details_1_2',
-                            3: 'league_log_details_0_2',
-                            4: 'league_log_details_2_1',
-                            5: None}
-        for (index, yml_title) in log_details_hash.items():
-            league_log = LeagueLog.find_or_initialize(league.id, user.id, users[index].id)
-            league_log.save()
-            if yml_title:
-                DataCreator().create(yml_title, overrided_dict={'league_log_id': league_log.id})
 
         self.login(admin_user.login_name, 'password')
         assert league.league_point_group_id is None
 
-        # 連勝記録
-        mocker.patch.object(ContinuousPoint, 'count_for_bonus', 3)
-
-        result = self.post(URL_PATH.format(league.id), {"league_point_group_id": 1})
+        result = self.post(URL_PATH.format(league.id), {"league_point_group_id": 1, "should_notify": 0})
         assert result.status_code == 302
-        point_ranks = PointRank.make_point_rank_list(league)
-        expected_hash_list = [
-            {'user_id': users[2].id, 'Total': 6000, 'LeaguePoint': [3000], 'BonusPoint': 1500, 'ContinuousPoint': 1500},
-            {'user_id': users[0].id, 'Total': 2000, 'LeaguePoint': [2000], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': users[3].id, 'Total': 1500, 'LeaguePoint': [1500], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': users[5].id, 'Total': 1200, 'LeaguePoint': [1200], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': users[4].id, 'Total': 800, 'LeaguePoint': [800], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': users[1].id, 'Total': 700, 'LeaguePoint': [700], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': admin_user.id, 'Total': 0, 'LeaguePoint': [], 'BonusPoint': 0, 'ContinuousPoint': 0},
-            {'user_id': guest.id, 'Total': 0, 'LeaguePoint': [], 'BonusPoint': 0, 'ContinuousPoint': 0},
-        ]
-        for i, point_rank in enumerate(point_ranks):
-            expected_hash = expected_hash_list[i]
-            assert point_rank.user.id == expected_hash['user_id']
-            assert point_rank.current_point == expected_hash['Total']
-            assert point_rank.current_league_points == expected_hash['LeaguePoint']
-            assert point_rank.current_league_points == expected_hash['LeaguePoint']
-            assert point_rank.current_league_points == expected_hash['LeaguePoint']
-            assert point_rank.current_bonus_point == expected_hash['BonusPoint']
-            assert point_rank.current_bonus_point == expected_hash['ContinuousPoint']
-        assert league.league_point_group_id == 1
-        assert LeagueStatus(league.status) == LeagueStatus.FINISHED
 
     def test_invalid_group_id(self):
         AddLeaguePoint().execute()
