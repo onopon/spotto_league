f66f2d95ebddcc03ad690d50fb7143a570c74bb8
kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>
2022-01-14 02:32:39 +0900

    percent to permil
    Signed-off-by: kiyoto.onozawa <kiyoto.onozawa@mixi.co.jp>

--- spotto_league/scripts/take_over_point.py
+++ spotto_league/scripts/take_over_point.py
@@ -10,12 +10,12 @@ class TakeOverPoint(Base):
     def execute_task(self, **kwargs) -> None:
         try:
             from_year = int(kwargs["from_year"])
-            percent = int(kwargs["percent"])
+            permil = int(kwargs["permil"])
             from_point_rank_list = PointRank.make_point_rank_list_in_season(from_year)
             for point_rank in from_point_rank_list:
                 if not (point_rank.user.is_admin() or point_rank.user.is_member()):
                     continue
-                point = int(Decimal(point_rank.current_point * percent / 100).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
+                point = int(Decimal(point_rank.current_point * permil / 1000).quantize(Decimal('0'), rounding=ROUND_HALF_UP))
                 up = UserPoint()
                 up.user_id = point_rank.user.id
                 up.set_point(point, POINT_HASH_KEY_BASE, "Take Over from {}".format(from_year))
@@ -27,15 +27,15 @@ class TakeOverPoint(Base):
 if __name__ == "__main__":
     """
     使用例)
-    2021年のポイントを今年に8%引き継ぐ場合
-    poetry run python -m spotto_league.scripts.take_over_point --from_year=2021 --percent=8
+    2021年のポイントを今年に12.5%引き継ぐ場合(permilは1000分率です）
+    poetry run python -m spotto_league.scripts.take_over_point --from_year=2021 --permil=125
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--from_year", required=True, type=int)
-    parser.add_argument("--percent", required=True, type=int)
+    parser.add_argument("--permil", required=True, type=int)
     args = parser.parse_args()
     TakeOverPoint().execute(
         from_year=args.from_year,
-        percent=args.percent
+        permil=args.permil
     )
     print("Done")
